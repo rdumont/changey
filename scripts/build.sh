96e0f4b0af04a44fbe1f3d116c5bf291b8100d51
Rodrigo Dumont <rlsdumont@gmail.com>
2014-06-16 15:38:37 -0300

    Try using global coffee installation

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ echo 'Removing old compiled .js files...'
 rm -f lib/*.js
 rm -f bin/*.js
 echo 'Compiling coffee files...'
-node_modules/.bin/coffee -b -c lib/ bin/
+coffee -b -c lib/ bin/
 echo 'Prepending shebang to bin file...'
 sed -i '1s;^;#!/usr/bin/env node\n;' bin/cli.js
